7cd24a001edd47c65c3bc4b9277efea59bf9013b
Trevor <trevorperry143@gmail.com>
2022-08-30 00:15:08 -0500

    commit tree 40f517f018926e76865c92a50d71f84acc0c4e7e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+const employee = {name: "Sam",
+streetAddress: "none"}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  })
+
+function updateEmployeeWithKeyAndValue(employee) {
+    return employee = {name: "Sam",
+streetAddress: "11 Broadway"}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee.streetAddress = "12 Broadway"
+    return employee = {name: "Sam",
+streetAddress: "12 Broadway"}
+}
+    
+    
+function deleteFromEmployeeByKey(employee) {
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name 
+    return employee
+}
